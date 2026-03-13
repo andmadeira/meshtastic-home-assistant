41f16ab6781a42ed8acc9f26393af0d1f750e49b
andmadeira <and.madeira@gmail.com>
2026-03-13 12:52:59 +0000

    fix logbook

--- custom_components/meshtastic/logbook.py
+++ custom_components/meshtastic/logbook.py
@@ -167,6 +167,11 @@ async def async_setup_message_logger(hass: HomeAssistant, entry: MeshtasticConfi
 
             hass.bus.async_fire(event_type=EVENT_MESHTASTIC_DOMAIN_EVENT, event_data=domain_event_data)
 
+        rx_snr = data["rx_snr"]
+        rx_rssi = data["rx_rssi"]
+        hop_start = data["hop_start"]
+        hop_limit = data["hop_limit"]
+
         if to_dm_entity_id or to_channel_entity_id:
             _publish_message_log_event(
                 hass,
