4468c37fb0b1cc29765ff1d31cbe368541eb8452
andmadeira <and.madeira@gmail.com>
2026-03-13 13:13:45 +0000

    fix api

--- custom_components/meshtastic/api.py
+++ custom_components/meshtastic/api.py
@@ -277,8 +277,8 @@ class MeshtasticApiClient:
                 "to": {"node": to_node, "channel": to_channel},
                 "gateway": self.get_own_node()["num"],
                 "message": packet.app_payload,
-                "snr": packet.rx_snr,
-                "rssi": packet.rx_rssi,
+                "rx_snr": packet.rx_snr,
+                "rx_rssi": packet.rx_rssi,
                 "hop_start": packet.hop_start,
                 "hop_limit": packet.hop_limit
             },
