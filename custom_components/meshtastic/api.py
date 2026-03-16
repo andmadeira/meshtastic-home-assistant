fdac43cf0f37af3a079110015832cf9b2e275745
andmadeira <and.madeira@gmail.com>
2026-03-16 22:34:06 +0000

    add via_mqtt

--- custom_components/meshtastic/api.py
+++ custom_components/meshtastic/api.py
@@ -317,6 +317,11 @@ class MeshtasticApiClient:
                 "to": {"node": to_node, "channel": to_channel},
                 "gateway": self.get_own_node()["num"],
                 "message": packet.app_payload,
+                "via_mqtt": packet.via_mqtt,
+                "rx_snr": packet.rx_snr,
+                "rx_rssi": packet.rx_rssi,
+                "hop_start": packet.hop_start,
+                "hop_limit": packet.hop_limit
             },
         )
 
